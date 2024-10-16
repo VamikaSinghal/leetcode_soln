c7d1c8177df5b1c63c36018d7a1e6359415586b4
Vamika Singhal <61896668+VamikaSinghal@users.noreply.github.com>
2024-10-16 12:37:58 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -14,4 +14,15 @@ class Solution(object):
             hashset.add(n)
         return False
 
+#242. Valid Anagram
+#Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        return sorted(s)==sorted(t)
         
