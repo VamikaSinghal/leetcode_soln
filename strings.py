6b65834611966792b11db86111dc888eac3dee73
Vamika Singhal <61896668+VamikaSinghal@users.noreply.github.com>
2024-10-18 11:27:15 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -17,3 +17,15 @@ class Solution(object):
                 if not prefix:
                     return ""
         return prefix
+
+#58. length of last string
+#Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.split()
+        return len(words[-1])
