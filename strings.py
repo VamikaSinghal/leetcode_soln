43aecbb61983c50363e416c533f2ee2d3f2a1558
Vamika Singhal <61896668+VamikaSinghal@users.noreply.github.com>
2024-10-19 23:12:07 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -29,3 +29,6 @@ class Solution(object):
         """
         words = s.split()
         return len(words[-1])
+#28. Find the Index of the First Occurrence in a String
+#Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
