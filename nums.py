95fe5e3f7aebc5d59a5dfe75f21f9754327afe49
Vamika Singhal <61896668+VamikaSinghal@users.noreply.github.com>
2024-10-20 22:41:33 +0530

    Update nums.py

--- nums.py
+++ nums.py
@@ -90,3 +90,26 @@ class Solution(object):
                 return i
         
         return len(nums)
+
+#66. Plus one
+#You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+#Increment the large integer by one and return the resulting array of digits.
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        #last to first
+        for i in range(len(digits)-1, -1, -1):
+            #if 9, set to 0
+            if digits[i]==9:
+                digits[i]=0
+            else:
+                #if not 9
+                digits[i] = digits[i]+1
+                return digits
+        
+        #all digits are 9
+        return [1]+digits
