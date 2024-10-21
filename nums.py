fa908844a1b006a44bad7b60abe1aec90c27c40d
Vamika Singhal <61896668+VamikaSinghal@users.noreply.github.com>
2024-10-21 20:37:46 +0530

    Update nums.py

--- nums.py
+++ nums.py
@@ -113,3 +113,26 @@ class Solution(object):
         
         #all digits are 9
         return [1]+digits
+
+#69. Sqrt(x)
+#Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+#You must not use any built-in exponent function or operator.
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left = 0
+        right = x
+        while left<=right :
+            mid = (right+left)//2
+
+            if (mid*mid)<x:
+                left = mid+1
+            elif (mid*mid)>x:
+                right= mid-1
+            else:
+                return mid
+            
+        return right
